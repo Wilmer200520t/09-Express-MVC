965baec615945c715022a02ac99b83a3c0ae7ffb
wilmer200520t <wilmer.2030@hotmail.com>
2023-11-03 12:04:26 -0500

    Error-Last Update
    Controlador error

--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,10 @@
-const error404=(req,res)=>{
-
-}
+const error404 = (req, res) => {
+  res.status(404).render("error", {
+    title: "Error 404 not found",
+    message: "El recurso que estas buscando no existe.",
+  });
+};
 
 export default {
-    error404
-}
+  error404,
+};
